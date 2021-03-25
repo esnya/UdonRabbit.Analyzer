468ac3f3b1de2b058316305d8240628913cfb3b7
Mikazuki <mikazuki_fuyuno@outlook.com>
2021-03-25 23:59:54 +0900

    feat: load UDON assemblies from Analyzer

---
+++ Source/UdonRabbit.Analyzer/MethodIsNotExposedToUdon.cs
@@ -0,0 +1,110 @@
+﻿/*-------------------------------------------------------------------------------------------
+ * Copyright (c) Natsuneko. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *------------------------------------------------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+#pragma warning disable RS1026
+
+namespace UdonRabbit.Analyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class MethodIsNotExposedToUdon : DiagnosticAnalyzer
+    {
+        private const string ComponentId = "URA0001";
+        private const string Category = "Udon";
+        private const string HelpLinkUri = "https://docs.mochizuki.moe/udon-rabbit/packages/analyzer/analyzers/URA0001/";
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.URA0001Title), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.URA0001MessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.URA0001Description), Resources.ResourceManager, typeof(Resources));
+        private static readonly DiagnosticDescriptor RuleSet = new(ComponentId, Title, MessageFormat, Category, DiagnosticSeverity.Error, true, Description, HelpLinkUri);
+
+        private HashSet<string> exposedMethodSymbols;
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(RuleSet);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            // context.EnableConcurrentExecution();
+            Debugger.Launch();
+
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.RegisterSyntaxNodeAction(AnalyzeInvocation, SyntaxKind.InvocationExpression);
+        }
+
+        private void AnalyzeInvocation(SyntaxNodeAnalysisContext context)
+        {
+            var invocation = (InvocationExpressionSyntax) context.Node;
+
+            if (invocation.Expression is not MemberAccessExpressionSyntax member)
+            {
+                Debug.WriteLine("UDON DEBUGGER: invocation.Expression is not MemberAccessExpression");
+                return;
+            }
+
+            if (exposedMethodSymbols == null)
+                LoadSdkAssemblies(context);
+
+            var methodSymbol = context.SemanticModel.GetSymbolInfo(invocation);
+            if (methodSymbol.Symbol == null)
+                return;
+
+            if (methodSymbol.Symbol is not IMethodSymbol method)
+                return;
+
+            if (method.Name == "EmitInteract")
+            {
+                Debug.WriteLine("UDON DEBUGGER: Report Diagnostic");
+                context.ReportDiagnostic(Diagnostic.Create(RuleSet, invocation.GetLocation(), method.Name));
+            }
+        }
+
+        private void LoadSdkAssemblies(SyntaxNodeAnalysisContext context)
+        {
+            var reference = context.Compilation.ExternalReferences.FirstOrDefault(w => w.Display.Contains("VRC.Udon.Common.dll"));
+            if (reference == null)
+                return; // The VRChat Udon SDK is not included in the workspace.
+
+            string FindUnityAssetsDirectory(string path)
+            {
+                return path.Substring(0, path.IndexOf("Assets", StringComparison.InvariantCulture));
+            }
+
+            var assemblies = Path.GetFullPath(Path.Combine(FindUnityAssetsDirectory(reference.Display), "Library", "ScriptAssemblies"));
+            var editor = Path.Combine(assemblies, "VRC.Udon.Editor.dll");
+            if (!File.Exists(editor))
+                return; // Invalid Path;
+
+            Assembly ResolveDynamicLoadingAssemblies(object _, ResolveEventArgs args)
+            {
+                var dll = $"{args.Name.Split(',').First()}.dll";
+                if (context.Compilation.ExternalReferences.Any(w => w.Display.Contains(dll)))
+                    return Assembly.LoadFrom(context.Compilation.ExternalReferences.First(w => w.Display.Contains(dll)).Display);
+                return Assembly.LoadFrom(Path.GetFullPath(Path.Combine(assemblies, dll)));
+            }
+
+            AppDomain.CurrentDomain.AssemblyResolve += ResolveDynamicLoadingAssemblies;
+            var assembly = Assembly.LoadFrom(editor);
+            var manager = assembly.GetType("VRC.Udon.Editor.UdonEditorManager");
+
+            Debug.WriteLine(manager.GetType());
+        }
+
+        private string GetUdonNamedType(string name)
+        {
+            return name;
+        }
+    }
+}
