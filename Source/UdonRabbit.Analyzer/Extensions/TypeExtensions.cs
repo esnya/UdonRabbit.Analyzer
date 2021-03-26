9c2fab0bd5640ff90833f669cf294eb190fd1cbb
esnya <esnya@nekometer.com>
2021-03-26 13:43:56 +0900

    feat: support to report as error when call not exposed APIs

---
+++ Source/UdonRabbit.Analyzer/Extensions/TypeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UdonRabbit.Analyzer.Extensions
+{
+    public static class TypeExtensions
+    {
+        // https://stackoverflow.com/questions/6386202/get-type-name-without-any-generics-info
+        public static string GetNameWithoutGenericArity(this Type t)
+        {
+            if (!t.IsGenericType)
+                return t.Name;
+
+            var name = t.Name;
+            var index = name.IndexOf('`');
+            return index == -1 ? name : name.Substring(0, index);
+        }
+    }
+}
