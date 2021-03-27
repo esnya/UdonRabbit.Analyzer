44de9c189f8bd073eb206b0858383bf38aa23da4
Mikazuki <mikazuki_fuyuno@outlook.com>
2021-03-27 22:54:40 +0900

    feat: support URA0005

---
+++ Source/UdonRabbit.Analyzer.Test/NotSupportInheritingFromInterfacesTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+
+using UdonRabbit.Analyzer.Test.Infrastructure;
+
+using Xunit;
+
+namespace UdonRabbit.Analyzer.Test
+{
+    public class NotSupportInheritingFromInterfacesTest : DiagnosticVerifier<NotSupportInheritingFromInterfaces>
+    {
+        [Fact]
+        public async Task InheritingFromInterfacesHasDiagnosticsReport()
+        {
+            var diagnostic = ExpectDiagnostic(NotSupportInheritingFromInterfaces.ComponentId)
+                             .WithLocation(8, 5)
+                             .WithSeverity(DiagnosticSeverity.Error)
+                             .WithArguments("IDisposable");
+
+            const string source = @"
+using System;
+
+using UdonSharp;
+
+namespace UdonRabbit
+{
+    public class TestClass : UdonSharpBehaviour, IDisposable
+    {
+        private void Update()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source, diagnostic);
+        }
+
+        [Fact]
+        public async Task UnityISerializationCallbackReceiverIsNoDiagnosticsReport()
+        {
+            // NOTE: UdonSharpBehaviour is inheriting from UnityEngine.ISerializationCallbackReceiver on Editor
+            const string source = @"
+using UdonSharp;
+
+namespace UdonRabbit
+{
+    public class TestClass : UdonSharpBehaviour
+    {
+        private void Update()
+        {
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source);
+        }
+    }
+}
