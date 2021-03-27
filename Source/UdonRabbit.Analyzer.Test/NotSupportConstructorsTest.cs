f9f10ee253a170e8700dc43f5a93c919aadb627f
Mikazuki <mikazuki_fuyuno@outlook.com>
2021-03-27 23:26:26 +0900

    feat(ura0007): support URA0007 no allow constructor

---
+++ Source/UdonRabbit.Analyzer.Test/NotSupportConstructorsTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+
+using UdonRabbit.Analyzer.Test.Infrastructure;
+
+using Xunit;
+
+namespace UdonRabbit.Analyzer.Test
+{
+    public class NotSupportConstructorsTest : DiagnosticVerifier<NotSupportConstructors>
+    {
+        [Fact]
+        public async Task ClassHasConstructorHasDiagnosticsReport()
+        {
+            var diagnostic = ExpectDiagnostic(NotSupportConstructors.ComponentId)
+                             .WithLocation(8, 9)
+                             .WithSeverity(DiagnosticSeverity.Error);
+
+            const string source = @"
+using UdonSharp;
+
+namespace UdonRabbit
+{
+    public class TestClass : UdonSharpBehaviour
+    {
+        public TestClass()
+        {
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source, diagnostic);
+        }
+
+        [Fact]
+        public async Task NoDiagnosticsReport()
+        {
+            const string source = @"
+namespace UdonRabbit
+{
+    public class TestClass
+    {
+        public TestClass()
+        {
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source);
+        }
+    }
+}
