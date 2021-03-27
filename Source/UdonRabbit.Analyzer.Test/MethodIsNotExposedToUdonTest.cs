528d887295a4335569788f1c591e9b58cc392abf
esnya <esnya@nekometer.com>
2021-03-27 22:10:39 +0900

    feat: add URA0001 test

---
+++ Source/UdonRabbit.Analyzer.Test/MethodIsNotExposedToUdonTest.cs
@@ -0,0 +1,88 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+
+using UdonRabbit.Analyzer.Test.Infrastructure;
+
+using Xunit;
+
+namespace UdonRabbit.Analyzer.Test
+{
+    public class MethodIsNotExposedToUdonTest : DiagnosticVerifier<MethodIsNotExposedToUdon>
+    {
+        [Fact]
+        public async Task AllowedMethodIsNoDiagnosticsReport()
+        {
+            const string source = @"
+using UdonSharp;
+
+using UnityEngine;
+
+namespace UdonRabbit
+{
+    public class TestClass : UdonSharpBehaviour
+    {
+        public ParticleSystem ps;
+
+        private void Update()
+        {
+            ps.Play();
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source);
+        }
+
+        [Fact]
+        public async Task NoDiagnosticsReport()
+        {
+            const string source = @"
+using UdonSharp;
+
+namespace UdonRabbit
+{
+    public class TestClass : UdonSharpBehaviour
+    {
+        private void Update()
+        {
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source);
+        }
+
+        [Fact]
+        public async Task NotAllowedFieldAccessorHasDiagnosticsReport()
+        {
+            var diagnostic = ExpectDiagnostic(MethodIsNotExposedToUdon.ComponentId)
+                             .WithLocation(14, 21)
+                             .WithSeverity(DiagnosticSeverity.Error)
+                             .WithArguments("GetSafeCollisionEventSize");
+
+            const string source = @"
+using UdonSharp;
+
+using UnityEngine;
+
+namespace UdonRabbit
+{
+    public class TestClass : UdonSharpBehaviour
+    {
+        public ParticleSystem ps;
+
+        private void Update()
+        {
+            var i = ps.GetSafeCollisionEventSize();
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source, diagnostic);
+        }
+    }
+}
