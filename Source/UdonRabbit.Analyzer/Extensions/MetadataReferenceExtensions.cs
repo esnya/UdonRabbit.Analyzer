54dc4ea68441e1a9d6f00f24aadfd22eb1cbb9c2
Mikazuki <mikazuki_fuyuno@outlook.com>
2021-03-28 14:26:19 +0900

    fix: working correctly on Rider and OmniSharp

---
+++ Source/UdonRabbit.Analyzer/Extensions/MetadataReferenceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace UdonRabbit.Analyzer.Extensions
+{
+    public static class MetadataReferenceExtensions
+    {
+        public static string ToFilePath(this MetadataReference reference)
+        {
+            // JetBrains Rider/OmniSharp returns PortableExecutableReference
+            if (reference is PortableExecutableReference per)
+                return per.FilePath;
+
+            //But Microsoft Visual Studio returns SerializerService+SerializedMetadataReference
+            return reference.Display;
+        }
+    }
+}
