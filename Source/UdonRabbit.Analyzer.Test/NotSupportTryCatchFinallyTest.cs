e5993a388c274ff2d11fecf632eb2e73540110e8
esnya <esnya@nekometer.com>
2021-03-28 20:20:25 +0900

    feat: add URA0011 No Try Statement

---
+++ Source/UdonRabbit.Analyzer.Test/NotSupportTryCatchFinallyTest.cs
@@ -0,0 +1,108 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+
+using UdonRabbit.Analyzer.Test.Infrastructure;
+
+using Xunit;
+
+namespace UdonRabbit.Analyzer.Test
+{
+    public class NotSupportTryCatchFinallyTest : DiagnosticVerifier<NotSupportTryCatchFinally>
+    {
+        [Fact]
+        public async Task MonoBehaviourClassTryCatchStatementHasNoDiagnosticsReport()
+        {
+            const string source = @"
+using UnityEngine;
+
+namespace UdonRabbit
+{
+    public class TestClass : MonoBehaviour
+    {
+        private void Update()
+        {
+            object o = null;
+
+            try
+            {
+                var i = (int) o;
+            }
+            catch
+            {
+            }
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source);
+        }
+
+        [Fact]
+        public async Task UdonSharpBehaviourClassTryCatchStatementHasDiagnosticsReport()
+        {
+            var diagnostic = ExpectDiagnostic(NotSupportTryCatchFinally.ComponentId)
+                             .WithLocation(12, 13)
+                             .WithSeverity(DiagnosticSeverity.Error);
+
+            const string source = @"
+using UdonSharp;
+
+namespace UdonRabbit
+{
+    public class TestClass : UdonSharpBehaviour
+    {
+        private void Update()
+        {
+            object o = null;
+
+            try
+            {
+                var i = (int) o;
+            }
+            catch
+            {
+            }
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source, diagnostic);
+        }
+
+        [Fact]
+        public async Task UdonSharpBehaviourClassTryFinallyStatementHasDiagnosticsReport()
+        {
+            var diagnostic = ExpectDiagnostic(NotSupportTryCatchFinally.ComponentId)
+                             .WithLocation(12, 13)
+                             .WithSeverity(DiagnosticSeverity.Error);
+
+            const string source = @"
+using UdonSharp;
+
+namespace UdonRabbit
+{
+    public class TestClass : UdonSharpBehaviour
+    {
+        private void Update()
+        {
+            object o = null;
+
+            try
+            {
+                var i = (int) o;
+            }
+            finally
+            {
+            }
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source, diagnostic);
+        }
+    }
+}
