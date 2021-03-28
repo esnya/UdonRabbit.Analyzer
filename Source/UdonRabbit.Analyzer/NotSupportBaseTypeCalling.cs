0f31a593444a596c37d5a540267b244272f59b75
Mikazuki <mikazuki_fuyuno@outlook.com>
2021-03-28 12:58:35 +0900

    feat: add URA0009

---
+++ Source/UdonRabbit.Analyzer/NotSupportBaseTypeCalling.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Immutable;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using UdonRabbit.Analyzer.Udon;
+
+namespace UdonRabbit.Analyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class NotSupportBaseTypeCalling : DiagnosticAnalyzer
+    {
+        public const string ComponentId = "URA0009";
+        private const string Category = UdonConstants.UdonCategory;
+        private const string HelpLinkUri = "https://docs.mochizuki.moe/udon-rabbit/packages/analyzer/analyzers/URA0009/";
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.URA0009Title), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.URA0009MessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.URA0009Description), Resources.ResourceManager, typeof(Resources));
+        private static readonly DiagnosticDescriptor RuleSet = new(ComponentId, Title, MessageFormat, Category, DiagnosticSeverity.Error, true, Description, HelpLinkUri);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(RuleSet);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.RegisterSyntaxNodeAction(AnalyzeBaseExpression, SyntaxKind.BaseExpression);
+        }
+
+        private static void AnalyzeBaseExpression(SyntaxNodeAnalysisContext context)
+        {
+            var expression = (BaseExpressionSyntax) context.Node;
+            if (!UdonSharpBehaviourUtility.ShouldAnalyzeSyntax(context.SemanticModel, expression))
+                return;
+
+            context.ReportDiagnostic(Diagnostic.Create(RuleSet, expression.GetLocation()));
+        }
+    }
+}
