629896371a2f233c91f160c39b1924c0b5a31236
esnya <esnya@nekometer.com>
2021-03-29 20:24:50 +0900

    feat: add URA0019 no ref parameter in user defined method decl

---
+++ Source/UdonRabbit.Analyzer/NotSupportRefParametersInUserDefinedMethods.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using UdonRabbit.Analyzer.Udon;
+
+namespace UdonRabbit.Analyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class NotSupportRefParametersInUserDefinedMethods : DiagnosticAnalyzer
+    {
+        public const string ComponentId = "URA0019";
+        private const string Category = UdonConstants.UdonSharpCategory;
+        private const string HelpLinkUri = "https://github.com/esnya/UdonRabbit.Analyzer/blob/master/docs/analyzers/URA0019.md";
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.URA0019Title), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.URA0019MessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.URA0019Description), Resources.ResourceManager, typeof(Resources));
+        private static readonly DiagnosticDescriptor RuleSet = new(ComponentId, Title, MessageFormat, Category, DiagnosticSeverity.Error, true, Description, HelpLinkUri);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(RuleSet);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.RegisterSyntaxNodeAction(AnalyzeParameterList, SyntaxKind.ParameterList);
+        }
+
+        private static void AnalyzeParameterList(SyntaxNodeAnalysisContext context)
+        {
+            var parameters = (ParameterListSyntax) context.Node;
+            if (!UdonSharpBehaviourUtility.ShouldAnalyzeSyntax(context.SemanticModel, parameters))
+                return;
+
+            foreach (var parameter in parameters.Parameters.Where(parameter => parameter.Modifiers.Any(SyntaxKind.RefKeyword)))
+                context.ReportDiagnostic(Diagnostic.Create(RuleSet, parameter.GetLocation()));
+        }
+    }
+}
