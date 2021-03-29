dd5b9dbe12277a4b2cb0ea62922829d13b293b59
Mikazuki <mikazuki_fuyuno@outlook.com>
2021-03-29 11:41:01 +0900

    feat: add URA0013 no multidimensional arrays

---
+++ Source/UdonRabbit.Analyzer/NotSupportMultidimentionalArrays.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Immutable;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using UdonRabbit.Analyzer.Udon;
+
+namespace UdonRabbit.Analyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class NotSupportMultidimentionalArrays : DiagnosticAnalyzer
+    {
+        public const string ComponentId = "URA0013";
+        private const string Category = UdonConstants.UdonSharpCategory;
+        private const string HelpLinkUri = "https://github.com/mika-f/UdonRabbit.Analyzer/blob/master/docs/analyzers/URA0013.md";
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.URA0013Title), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.URA0013MessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.URA0013Description), Resources.ResourceManager, typeof(Resources));
+        private static readonly DiagnosticDescriptor RuleSet = new(ComponentId, Title, MessageFormat, Category, DiagnosticSeverity.Error, true, Description, HelpLinkUri);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(RuleSet);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.RegisterSyntaxNodeAction(AnalyzeVariableDeclaration, SyntaxKind.VariableDeclaration);
+            context.RegisterSyntaxNodeAction(AnalyzeParameter, SyntaxKind.Parameter);
+        }
+
+        private static void AnalyzeVariableDeclaration(SyntaxNodeAnalysisContext context)
+        {
+            var declaration = (VariableDeclarationSyntax) context.Node;
+            if (!UdonSharpBehaviourUtility.ShouldAnalyzeSyntax(context.SemanticModel, declaration))
+                return;
+
+            foreach (var variable in declaration.Variables)
+            {
+                var s = context.SemanticModel.GetDeclaredSymbol(variable);
+                if (s is IFieldSymbol f && IsDimensionalArrayType(f.Type))
+                    context.ReportDiagnostic(Diagnostic.Create(RuleSet, variable.GetLocation()));
+
+                if (s is ILocalSymbol l && IsDimensionalArrayType(l.Type))
+                    context.ReportDiagnostic(Diagnostic.Create(RuleSet, variable.GetLocation()));
+
+                if (s is IParameterSymbol m && IsDimensionalArrayType(m.Type))
+                    context.ReportDiagnostic(Diagnostic.Create(RuleSet, variable.GetLocation()));
+            }
+        }
+
+        private static void AnalyzeParameter(SyntaxNodeAnalysisContext context)
+        {
+            var parameter = (ParameterSyntax) context.Node;
+            if (!UdonSharpBehaviourUtility.ShouldAnalyzeSyntax(context.SemanticModel, parameter))
+                return;
+
+            var s = context.SemanticModel.GetDeclaredSymbol(parameter);
+            if (IsDimensionalArrayType(s.Type))
+                context.ReportDiagnostic(Diagnostic.Create(RuleSet, parameter.GetLocation()));
+        }
+
+        private static bool IsDimensionalArrayType(ITypeSymbol s)
+        {
+            if (s.TypeKind != TypeKind.Array || s is not IArrayTypeSymbol t)
+                return false;
+
+            return t.Rank >= 2;
+        }
+    }
+}
