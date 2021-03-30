e0c573fc08dbd69709510c71366ec1c0b699b862
esnya <esnya@nekometer.com>
2021-03-30 11:09:22 +0900

    feat: add URA0020 no allow is keyword

---
+++ Source/UdonRabbit.Analyzer.Test/NotSupportIsKeywordInExpressionTest.cs
@@ -0,0 +1,133 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+
+using UdonRabbit.Analyzer.Test.Infrastructure;
+
+using Xunit;
+
+namespace UdonRabbit.Analyzer.Test
+{
+    public class NotSupportIsKeywordInExpressionTest : DiagnosticVerifier<NotSupportIsKeywordInExpression>
+    {
+        [Fact]
+        public async Task MonoBehaviourIsExpressionHasNoDiagnosticsReport()
+        {
+            const string source = @"
+using UnityEngine;
+
+namespace UdonRabbit
+{
+    public class TestBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private Component _component;
+
+        private void Start()
+        {
+            if (_component is Transform)
+            {
+                var t = (Transform) _component;
+            }
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source);
+        }
+
+        [Fact]
+        public async Task MonoBehaviourIsPatternExpressionHasNoDiagnosticsReport()
+        {
+            const string source = @"
+using UnityEngine;
+
+namespace UdonRabbit
+{
+    public class TestBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private Component _component;
+
+        private void Start()
+        {
+            if (_component is Transform t)
+            {
+                var position = t.position;
+            }
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source);
+        }
+
+        [Fact]
+        public async Task UdonSharpBehaviourIsExpressionHasDiagnosticsReport()
+        {
+            var diagnostic = ExpectDiagnostic(NotSupportIsKeywordInExpression.ComponentId)
+                             .WithLocation(15, 17)
+                             .WithSeverity(DiagnosticSeverity.Error);
+
+            const string source = @"
+using UdonSharp;
+
+using UnityEngine;
+
+namespace UdonRabbit
+{
+    public class TestBehaviour : UdonSharpBehaviour
+    {
+        [SerializeField]
+        private Component _component;
+
+        private void Start()
+        {
+            if (_component is Transform)
+            {
+                var t = (Transform) _component;
+            }
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source, diagnostic);
+        }
+
+        [Fact]
+        public async Task UdonSharpBehaviourIsPatternExpressionHasDiagnosticsReport()
+        {
+            var diagnostic = ExpectDiagnostic(NotSupportIsKeywordInExpression.ComponentId)
+                             .WithLocation(15, 17)
+                             .WithSeverity(DiagnosticSeverity.Error);
+
+            const string source = @"
+using UdonSharp;
+
+using UnityEngine;
+
+namespace UdonRabbit
+{
+    public class TestBehaviour : UdonSharpBehaviour
+    {
+        [SerializeField]
+        private Component _component;
+
+        private void Start()
+        {
+            if (_component is Transform t)
+            {
+                var position = t.position;
+            }
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source, diagnostic);
+        }
+    }
+}
---
+++ Source/UdonRabbit.Analyzer/NotSupportIsKeywordInExpression.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Immutable;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using UdonRabbit.Analyzer.Udon;
+
+namespace UdonRabbit.Analyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class NotSupportIsKeywordInExpression : DiagnosticAnalyzer
+    {
+        public const string ComponentId = "URA0020";
+        private const string Category = UdonConstants.UdonCategory;
+        private const string HelpLinkUri = "https://github.com/esnya/UdonRabbit.Analyzer/blob/master/docs/analyzers/URA0020.md";
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.URA0020Title), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.URA0020MessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.URA0020Description), Resources.ResourceManager, typeof(Resources));
+        private static readonly DiagnosticDescriptor RuleSet = new(ComponentId, Title, MessageFormat, Category, DiagnosticSeverity.Error, true, Description, HelpLinkUri);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(RuleSet);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.RegisterSyntaxNodeAction(AnalyzeIsExpression, SyntaxKind.IsExpression);
+            context.RegisterSyntaxNodeAction(AnalyzeIsPatternExpression, SyntaxKind.IsPatternExpression);
+        }
+
+        private static void AnalyzeIsExpression(SyntaxNodeAnalysisContext context)
+        {
+            var expression = (BinaryExpressionSyntax) context.Node;
+            if (!UdonSharpBehaviourUtility.ShouldAnalyzeSyntax(context.SemanticModel, expression))
+                return;
+
+            if (expression.Kind() == SyntaxKind.IsExpression)
+                context.ReportDiagnostic(Diagnostic.Create(RuleSet, expression.GetLocation()));
+        }
+
+        private static void AnalyzeIsPatternExpression(SyntaxNodeAnalysisContext context)
+        {
+            var expression = (IsPatternExpressionSyntax) context.Node;
+            if (!UdonSharpBehaviourUtility.ShouldAnalyzeSyntax(context.SemanticModel, expression))
+                return;
+
+            context.ReportDiagnostic(Diagnostic.Create(RuleSet, expression.GetLocation()));
+        }
+    }
+}
