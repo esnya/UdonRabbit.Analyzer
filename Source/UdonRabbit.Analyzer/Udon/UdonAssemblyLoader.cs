d0ef4ed271cb28935cb12a4ef67a642078a8c8c3
Mikazuki <mikazuki_fuyuno@outlook.com>
2021-04-01 00:17:52 +0900

    fix: fixed the problem that the class belonging to VRC.SDKBase causes an error

--- Source/UdonRabbit.Analyzer/Udon/UdonAssemblyLoader.cs
+++ Source/UdonRabbit.Analyzer/Udon/UdonAssemblyLoader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 
@@ -13,6 +14,11 @@ namespace UdonRabbit.Analyzer.Udon
     {
         private static readonly object LockObj = new();
 
+        private static readonly HashSet<string> UnityAssemblies = new()
+        {
+            "UnityEngine.UI"
+        };
+
         private static readonly HashSet<string> UdonAllowAssemblies = new()
         {
             "VRCSDK3",
@@ -22,9 +28,17 @@ namespace UdonRabbit.Analyzer.Udon
             "Unity.TextMeshPro"
         };
 
+        private static readonly HashSet<string> UdonExternalAssemblies = new()
+        {
+            "VRC.Udon.VRCGraphModules",
+            "VRC.Udon.VRCTypeResolverModules"
+        };
+
         public static bool IsAssemblyLoaded { get; private set; }
 
         public static Assembly UdonAssembly { get; private set; }
+
+        public static HashSet<string> LoadedAssemblies { get; } = new();
 
         private static IEnumerable<string> FindAssetsDirectoryFromPath(string path)
         {
@@ -59,18 +73,22 @@ namespace UdonRabbit.Analyzer.Udon
                     if (name.EndsWith(".resources"))
                         return null;
 
+                    LoadedAssemblies.Add(name);
                     if (references.Any(w => w.ToFilePath().EndsWith($"{name}.dll")))
                         return Assembly.LoadFrom(references.First(w => w.ToFilePath().EndsWith($"{name}.dll")).ToFilePath());
                     return Assembly.LoadFrom(resolver.Resolve($"{name}.dll"));
                 };
 
-                Assembly.LoadFrom(resolver.Resolve("VRC.Udon.Editor.dll"));
-                foreach (var allowed in UdonAllowAssemblies)
-                    Assembly.LoadFrom(resolver.Resolve($"{allowed}.dll"));
-
-                var assembly = AppDomain.CurrentDomain.GetAssemblies().First(w => w.GetName().Name == "VRC.Udon.Editor");
+                var assembly = Assembly.LoadFrom(resolver.Resolve("VRC.Udon.Editor.dll"));
                 if (assembly.GetType("VRC.Udon.Editor.UdonEditorManager") == null)
                     return;
+
+                foreach (var reference in references.Where(w => Path.GetFileNameWithoutExtension(w.ToFilePath()).Contains("UnityEngine")))
+                    AppDomain.CurrentDomain.Load(AssemblyName.GetAssemblyName(reference.ToFilePath()));
+                foreach (var external in UdonExternalAssemblies)
+                    AppDomain.CurrentDomain.Load(AssemblyName.GetAssemblyName(resolver.Resolve($"{external}.dll")));
+                foreach (var allowed in UdonAllowAssemblies)
+                    AppDomain.CurrentDomain.Load(AssemblyName.GetAssemblyName(resolver.Resolve($"{allowed}.dll")));
 
                 UdonAssembly = assembly;
                 IsAssemblyLoaded = true;
