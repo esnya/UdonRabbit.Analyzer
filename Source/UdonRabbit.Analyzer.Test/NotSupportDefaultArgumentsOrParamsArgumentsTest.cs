2d66286d843c6784c845bce91252494294ecd16f
Mikazuki <mikazuki_fuyuno@outlook.com>
2021-03-31 17:16:26 +0900

    feat: add URA0024 no allow `params` and default parameter in method declaration

---
+++ Source/UdonRabbit.Analyzer.Test/NotSupportDefaultArgumentsOrParamsArgumentsTest.cs
@@ -0,0 +1,121 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+
+using UdonRabbit.Analyzer.Test.Infrastructure;
+
+using Xunit;
+
+namespace UdonRabbit.Analyzer.Test
+{
+    public class NotSupportDefaultArgumentsOrParamsArgumentsTest : DiagnosticVerifier<NotSupportDefaultArgumentsOrParamsArguments>
+    {
+        [Fact]
+        public async Task MonoBehaviourDefaultArgumentHasNoDiagnosticsReport()
+        {
+            const string source = @"
+using UnityEngine;
+
+namespace UdonRabbit
+{
+    public class TestBehaviour : MonoBehaviour
+    {
+        private void TestMethod(string str, string area = """")
+        {
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source);
+        }
+
+        [Fact]
+        public async Task MonoBehaviourParamsArgumentHasNoDiagnosticsReport()
+        {
+            const string source = @"
+using UnityEngine;
+
+namespace UdonRabbit
+{
+    public class TestBehaviour : MonoBehaviour
+    {
+        private void TestMethod(string str, params string[] options)
+        {
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source);
+        }
+
+        [Fact]
+        public async Task UdonSharpBehaviourArgumentHasNoDiagnosticsReport()
+        {
+            const string source = @"
+using UdonSharp;
+
+namespace UdonRabbit
+{
+    public class TestBehaviour : UdonSharpBehaviour
+    {
+        private void TestMethod(string str)
+        {
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source);
+        }
+
+        [Fact]
+        public async Task UdonSharpBehaviourDefaultArgumentHasNoDiagnosticsReport()
+        {
+            var diagnostic = ExpectDiagnostic(NotSupportDefaultArgumentsOrParamsArguments.ComponentId)
+                             .WithLocation(8, 45)
+                             .WithSeverity(DiagnosticSeverity.Error);
+
+            const string source = @"
+using UdonSharp;
+
+namespace UdonRabbit
+{
+    public class TestBehaviour : UdonSharpBehaviour
+    {
+        private void TestMethod(string str, string area = """")
+        {
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source, diagnostic);
+        }
+
+        [Fact]
+        public async Task UdonSharpBehaviourParamsArgumentHasNoDiagnosticsReport()
+        {
+            var diagnostic = ExpectDiagnostic(NotSupportDefaultArgumentsOrParamsArguments.ComponentId)
+                             .WithLocation(8, 45)
+                             .WithSeverity(DiagnosticSeverity.Error);
+
+            const string source = @"
+using UdonSharp;
+
+namespace UdonRabbit
+{
+    public class TestBehaviour : UdonSharpBehaviour
+    {
+        private void TestMethod(string str, params string[] options)
+        {
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source, diagnostic);
+        }
+    }
+}
