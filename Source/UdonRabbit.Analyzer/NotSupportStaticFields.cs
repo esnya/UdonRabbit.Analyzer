f416617c4fb0321ad4faa129c26fc6bc725e9145
Mikazuki <mikazuki_fuyuno@outlook.com>
2021-03-31 17:48:54 +0900

    fix: fix message

--- Source/UdonRabbit.Analyzer/NotSupportStaticFields.cs
+++ Source/UdonRabbit.Analyzer/NotSupportStaticFields.cs
@@ -15,9 +15,9 @@ namespace UdonRabbit.Analyzer
         public const string ComponentId = "URA0025";
         private const string Category = UdonConstants.UdonSharpCategory;
         private const string HelpLinkUri = "https://github.com/mika-f/UdonRabbit.Analyzer/blob/master/docs/analyzers/URA0025.md";
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.URA0024Title), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.URA0024MessageFormat), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.URA0024Description), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.URA0025Title), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.URA0025MessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.URA0025Description), Resources.ResourceManager, typeof(Resources));
         private static readonly DiagnosticDescriptor RuleSet = new(ComponentId, Title, MessageFormat, Category, DiagnosticSeverity.Error, true, Description, HelpLinkUri);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(RuleSet);
