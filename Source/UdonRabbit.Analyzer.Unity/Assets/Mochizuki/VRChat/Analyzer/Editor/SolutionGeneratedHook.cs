032b2f0422fd90b40d584ad80ccc576b97f9ba0d
Mikazuki <mikazuki_fuyuno@outlook.com>
2021-04-04 12:41:14 +0900

    chore: prepare for providing UnityPackage

---
+++ Source/UdonRabbit.Analyzer.Unity/Assets/Mochizuki/VRChat/Analyzer/Editor/SolutionGeneratedHook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Xml;
+using System.Xml.Linq;
+
+using UnityEditor;
+
+using UnityEngine;
+
+namespace Mochizuki.VRChat.Analyzer
+{
+    public class SolutionGeneratedHook : AssetPostprocessor
+    {
+        private const string Analyzer = "UdonRabbit.Analyzer";
+        private const string AnalyzerNamespace = "moe.mochizuki.vrchat.analyzer";
+        private const string XmlNamespace = "http://schemas.microsoft.com/developer/msbuild/2003";
+
+        private static string OnGeneratedCSProject(string path, string content)
+        {
+            var analyzerRoot = GetRelativePathForPackages();
+
+            var document = XDocument.Parse(content);
+            var @namespace = (XNamespace) XmlNamespace;
+
+            var itemGroup = new XElement(@namespace + "ItemGroup");
+            var analyzer = new XElement(@namespace + "Analyzer", new XAttribute("Include", Path.Combine(analyzerRoot, "Plugins", "UdonRabbit.Analyzer.dll")));
+            var codeFix = new XElement(@namespace + "Analyzer", new XAttribute("Include", Path.Combine(analyzerRoot, "Plugins", "UdonRabbit.CodeFix.dll")));
+            
+            itemGroup.Add(analyzer);
+            itemGroup.Add(codeFix);
+            document.Root?.Add(itemGroup);
+
+            return document.Declaration + Environment.NewLine + document.Root;
+        }
+
+        private static string GetRelativePathForPackages([CallerFilePath] string path = "")
+        {
+            var root = Path.GetFullPath(Application.dataPath);
+            var analyzer = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(path), ".."));
+            return (new Uri(root)).MakeRelativeUri(new Uri(analyzer)).ToString();
+        }
+    }
+}
