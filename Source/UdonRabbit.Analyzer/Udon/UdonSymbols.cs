fe86c9a86c3feb2898b5037023f7b2c4e814856e
esnya <esnya@nekometer.com>
2021-04-06 08:57:02 +0900

    fix: fix method name of signature

--- Source/UdonRabbit.Analyzer/Udon/UdonSymbols.cs
+++ Source/UdonRabbit.Analyzer/Udon/UdonSymbols.cs
@@ -30,7 +30,7 @@ namespace UdonRabbit.Analyzer.Udon
             $"{UdonConstants.UdonSharpBehaviour}.__SendCustomEvent__SystemString__SystemObject",
             $"{UdonConstants.UdonSharpBehaviour}.__SendCustomNetworkEvent__VRCUdonCommonInterfacesNetworkEventTarget_SystemString__SystemVoid",
             $"{UdonConstants.UdonSharpBehaviour}.__SendCustomEventDelayedSeconds__SystemString__SystemSingle_VRCUdonCommonEnumsEventTiming__SystemVoid",
-            $"{UdonConstants.UdonSharpBehaviour}.__SendCustomEventDelayedSeconds__SystemString__SystemInt32_VRCUdonCommonEnumsEventTiming__SystemVoid",
+            $"{UdonConstants.UdonSharpBehaviour}.__SendCustomEventDelayedFrames__SystemString__SystemInt32_VRCUdonCommonEnumsEventTiming__SystemVoid",
             $"{UdonConstants.UdonSharpBehaviour}.__VRCInstantiate_UnityEngineGameObject__UnityEngineGameObject",
             $"{UdonConstants.UdonSharpBehaviour}.__RequestSerialization__SystemVoid"
 
