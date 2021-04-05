71b1125c97aa7e511224856d89f61e45a8d7ac99
esnya <esnya@nekometer.com>
2021-04-05 19:01:36 +0900

    fix: remove old extension path

--- Source/UdonRabbit.Analyzer.VisualStudioCode/src/omnisharp.ts
+++ Source/UdonRabbit.Analyzer.VisualStudioCode/src/omnisharp.ts
@@ -46,7 +46,7 @@ const enableOnWorkspace = (path: string): void => {
 
   const analyzers = join(extensionPath, "externals").replace(/\\/g, "/");
 
-  if (configuration.RoslynExtensionsOptions.LocationPaths.includes("esnya.udonrabbit-analyzer")) {
+  if (configuration.RoslynExtensionsOptions.LocationPaths.find((w) => w.includes("esnya.udonrabbit-analyzer"))) {
     const paths = configuration.RoslynExtensionsOptions.LocationPaths.filter((w) => !w.includes("esnya.udonrabbit-analyzer"));
     configuration.RoslynExtensionsOptions.LocationPaths = paths;
   }
