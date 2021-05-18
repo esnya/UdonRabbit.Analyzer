6ebf4c8d13b5e2d76bafbf5bd02d2ecb19e47dc5
esnya <esnya@nekometer.com>
2021-05-18 12:55:12 +0900

    fix: use temporary directory

--- Source/UdonRabbit.Analyzer/Udon/UdonAssemblyResolver.cs
+++ Source/UdonRabbit.Analyzer/Udon/UdonAssemblyResolver.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace UdonRabbit.Analyzer.Udon
 {
@@ -9,6 +11,7 @@ namespace UdonRabbit.Analyzer.Udon
         private readonly HashSet<string> _assemblies;
         private readonly Dictionary<string, bool> _paths;
         private readonly string _session;
+        private readonly string _unique;
         private string _sessionDir;
 
         public UdonAssemblyResolver(IEnumerable<string> paths)
@@ -17,6 +20,14 @@ namespace UdonRabbit.Analyzer.Udon
             _paths = paths.ToDictionary(w => w, _ => false);
 
             _session = $"SDK@{UdonAssemblyVersion.SDKVersion}-Udon@{UdonAssemblyVersion.UdonVersion}-UdonSharp@{UdonAssemblyVersion.UdonSharpVersion}";
+
+            var md5 = new MD5CryptoServiceProvider();
+            var sb = new StringBuilder();
+            var bytes = md5.ComputeHash(Encoding.UTF8.GetBytes(_paths.First().Key));
+            foreach (var b in bytes)
+                sb.Append(b.ToString("X2"));
+
+            _unique = sb.ToString();
         }
 
         public string Resolve(string name)
@@ -28,7 +39,7 @@ namespace UdonRabbit.Analyzer.Udon
 
             foreach (var path in dict.Where(w => !w.Value))
             {
-                var baseDir = string.IsNullOrEmpty(_sessionDir) ? Path.GetFullPath(Path.Combine(path.Key, "Analyzer", _session)) : _sessionDir;
+                var baseDir = string.IsNullOrEmpty(_sessionDir) ? Path.GetFullPath(Path.Combine(Path.GetTempPath(), "UdonRabbit.Analyzer", _unique, _session)) : _sessionDir;
                 if (!Directory.Exists(baseDir))
                 {
                     Directory.CreateDirectory(baseDir);
