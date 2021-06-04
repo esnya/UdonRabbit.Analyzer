ebcf296b4c3baae3173e388895e6cc3877d18aef
Natsuneko <me@mochizuki.moe>
2021-06-04 11:03:59 +0900

    fix: support nameof expression

--- Source/UdonRabbit.Analyzer.Test/MethodCalledByCustomNetworkEventMustBePublicTest.cs
+++ Source/UdonRabbit.Analyzer.Test/MethodCalledByCustomNetworkEventMustBePublicTest.cs
@@ -11,6 +11,99 @@ namespace UdonRabbit.Analyzer.Test
     public class MethodCalledByCustomNetworkEventMustBePublicTest : DiagnosticVerifier<MethodCalledByCustomNetworkEventMustBePublic>
     {
         [Fact]
+        public async Task UdonSharpBehaviourSendCustomNetworkEventByNameOfFullSignatureIsNonPublicHasDiagnosticsReport()
+        {
+            var diagnostic = ExpectDiagnostic(MethodCalledByCustomNetworkEventMustBePublic.ComponentId)
+                             .WithLocation(15, 9)
+                             .WithSeverity(DiagnosticSeverity.Warning);
+
+            const string source = @"
+using UdonSharp;
+
+using VRC.Udon.Common.Interfaces;
+
+namespace UdonRabbit
+{
+    public class TestBehaviour : UdonSharpBehaviour
+    {
+        private void Update()
+        {
+            SendCustomNetworkEvent(NetworkEventTarget.All, nameof(TestBehaviour.SomeNetworkEvent));
+        }
+
+        private void SomeNetworkEvent()
+        {
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source, diagnostic);
+        }
+
+        [Fact]
+        public async Task UdonSharpBehaviourSendCustomNetworkEventByNameOfSignatureIsNonPublicHasDiagnosticsReport()
+        {
+            var diagnostic = ExpectDiagnostic(MethodCalledByCustomNetworkEventMustBePublic.ComponentId)
+                             .WithLocation(15, 9)
+                             .WithSeverity(DiagnosticSeverity.Warning);
+
+            const string source = @"
+using UdonSharp;
+
+using VRC.Udon.Common.Interfaces;
+
+namespace UdonRabbit
+{
+    public class TestBehaviour : UdonSharpBehaviour
+    {
+        private void Update()
+        {
+            SendCustomNetworkEvent(NetworkEventTarget.All, nameof(SomeNetworkEvent));
+        }
+
+        private void SomeNetworkEvent()
+        {
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source, diagnostic);
+        }
+
+        [Fact]
+        public async Task UdonSharpBehaviourSendCustomNetworkEventIsNonPublicHasDiagnosticsReport()
+        {
+            var diagnostic = ExpectDiagnostic(MethodCalledByCustomNetworkEventMustBePublic.ComponentId)
+                             .WithLocation(15, 9)
+                             .WithSeverity(DiagnosticSeverity.Warning);
+
+            const string source = @"
+using UdonSharp;
+
+using VRC.Udon.Common.Interfaces;
+
+namespace UdonRabbit
+{
+    public class TestBehaviour : UdonSharpBehaviour
+    {
+        private void Update()
+        {
+            SendCustomNetworkEvent(NetworkEventTarget.All, ""SomeNetworkEvent"");
+        }
+
+        private void SomeNetworkEvent()
+        {
+        }
+    }
+}
+";
+
+            await VerifyAnalyzerAsync(source, diagnostic);
+        }
+
+        [Fact]
         public async Task UdonSharpBehaviourSendMethodCustomEventIsNonPublicHasDiagnosticsReport()
         {
             var diagnostic = ExpectDiagnostic(MethodCalledByCustomNetworkEventMustBePublic.ComponentId)
@@ -62,37 +155,6 @@ namespace UdonRabbit
 ";
 
             await VerifyAnalyzerAsync(source);
-        }
-
-        [Fact]
-        public async Task UdonSharpBehaviourSendMethodCustomNetworkEventIsNonPublicHasDiagnosticsReport()
-        {
-            var diagnostic = ExpectDiagnostic(MethodCalledByCustomNetworkEventMustBePublic.ComponentId)
-                             .WithLocation(15, 9)
-                             .WithSeverity(DiagnosticSeverity.Warning);
-
-            const string source = @"
-using UdonSharp;
-
-using VRC.Udon.Common.Interfaces;
-
-namespace UdonRabbit
-{
-    public class TestBehaviour : UdonSharpBehaviour
-    {
-        private void Update()
-        {
-            SendCustomNetworkEvent(NetworkEventTarget.All, ""SomeNetworkEvent"");
-        }
-
-        private void SomeNetworkEvent()
-        {
-        }
-    }
-}
-";
-
-            await VerifyAnalyzerAsync(source, diagnostic);
         }
     }
 }
