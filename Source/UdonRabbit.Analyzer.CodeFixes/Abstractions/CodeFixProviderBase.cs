cad3593e2308d71a4e81f103a04f8da49c2c9ae3
esnya <esnya@nekometer.com>
2021-06-11 14:14:09 +0900

    fix: support create method declaration in other class

---
+++ Source/UdonRabbit.Analyzer.CodeFixes/Abstractions/CodeFixProviderBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Text;
+
+namespace UdonRabbit.Analyzer.Abstractions
+{
+    public abstract class CodeFixProviderBase : CodeFixProvider
+    {
+        protected static bool TryFindFirstAncestorOrSelf<T>(SyntaxNode root, TextSpan span, out T r) where T : SyntaxNode
+        {
+            r = root.FindNode(span).FirstAncestorOrSelf<T>();
+
+            return r != null;
+        }
+
+        protected static CodeAction CreateCodeAction(string title, Func<CancellationToken, Task<Document>> callback, string equivalenceKey = null, params object[] arguments)
+        {
+            return CodeAction.Create(string.Format(title, arguments), callback, equivalenceKey);
+        }
+
+        protected static CodeAction CreateCodeAction(string title, Func<CancellationToken, Task<Solution>> callback, string equivalenceKey = null, params object[] arguments)
+        {
+            return CodeAction.Create(string.Format(title, arguments), callback, equivalenceKey);
+        }
+    }
+}
---
+++ Source/UdonRabbit.Analyzer.CodeFixes/Extensions/DocumentExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+
+namespace UdonRabbit.Analyzer.Extensions
+{
+    public static class DocumentExtensions
+    {
+        public static async Task<Document> ReplaceNode(this Document document, SyntaxNode oldNode, SyntaxNode newNode, CancellationToken cancellationToken = default)
+        {
+            var oldRoot = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            var newRoot = oldRoot.ReplaceNode(oldNode, newNode);
+
+            return document.WithSyntaxRoot(newRoot);
+        }
+    }
+}
